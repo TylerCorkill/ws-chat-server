ef8b99ddd19e706486c4b63395846e987b3e41f6
Tyler Corkill <TylerCorkill@gmail.com>
2022-05-14 15:57:59 -0700

    [master] Add console helpers

---
+++ src/helpers/log/log.ts
@@ -0,0 +1,11 @@
+export function log(...args: any[]): void {
+  console.log(...args) // eslint-disable-line
+}
+
+export function warn(...args: any[]): void {
+  console.warn(...args) // eslint-disable-line
+}
+
+export function error(...args: any[]): void {
+  console.error(...args) // eslint-disable-line
+}
